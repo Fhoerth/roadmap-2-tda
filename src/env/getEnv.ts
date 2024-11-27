29da63169a1956fc965ccc02507c4c8dfbf81f3a
Federico Hoerth <fedehoerth@CAPE-MACBOOK-PRO-13.local>
2024-11-27 04:21:24 -0300

    Add env

---
+++ src/env/getEnv.ts
@@ -0,0 +1,14 @@
+import { assert } from '../utils/assert';
+
+type Env = {
+  VITE_API_BASE_URL: string;
+};
+
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string | null;
+assert(VITE_API_BASE_URL);
+
+console.log(VITE_API_BASE_URL);
+
+export const env: Env = {
+  VITE_API_BASE_URL,
+};
