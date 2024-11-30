d89f68289462560dc8d20b1a99fe84d2a0be7090
Federico Hoerth <fedehoerth@CAPE-MACBOOK-PRO-13.local>
2024-11-30 05:45:28 -0300

    Run format

--- format.sh
+++ format.sh
@@ -1,2 +1,1 @@
-cd backend && ./format.sh && cd .. &&
-cd frontend && yarn config set nodeLinker node-modules && yarn install && ./format.sh && yarn config set nodeLinker pnp && yarn install && cd ..
+./install.sh && cd backend && ./format.sh && cd .. && cd frontend && ./format.sh && cd ..
