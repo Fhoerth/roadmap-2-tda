11a51eeb0d268717c1971570e787c9d132a0101b
Federico Hoerth <fedehoerth@CAPE-MACBOOK-PRO-13.local>
2024-11-30 18:33:10 -0300

    Add problems endpoint

---
+++ backend/src/main/java/tracker/controllers/ProblemsController.java
@@ -0,0 +1,20 @@
+package tracker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tracker.models.Category;
+import tracker.repositories.ProblemRepository;
+
+@RestController
+@RequestMapping("/api/problems")
+public class ProblemsController {
+  @Autowired
+  private ProblemRepository problemRepository;
+
+  @GetMapping
+  public List<Category> getAllCategories() {
+    return problemRepository.findAll();
+  }
+}
