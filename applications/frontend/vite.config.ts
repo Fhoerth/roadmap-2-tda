2c1449e5fb5037d563f675f3f49f915e69507b0c
Federico Hoerth <fedehoerth@CAPE-MACBOOK-PRO-13.local>
2024-12-02 04:01:51 -0300

    Change vite config to include env variable

--- applications/frontend/vite.config.ts
+++ applications/frontend/vite.config.ts
@@ -7,10 +7,17 @@ if (typeof process.env.PORT !== 'undefined') {
   port = parseInt(process.env.PORT);
 }
 
+console.log(process.env.VITE_BACKEND_URL);
+
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [react()],
   base: '/',
+  define: {
+    'process.env': {
+      'VITE_BACKEND_URL': process.env.VITE_BACKEND_URL || null,
+    },
+  },
   server: {
     port,
     host: '0.0.0.0',
