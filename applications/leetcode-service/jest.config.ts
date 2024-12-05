36d3bea58e291097cf9b4004579c74ad7a82ff4f
Fhoerth <federicohoerth@gmail.com>
2024-12-05 18:51:41 -0300

    Update jest config

--- applications/leetcode-service/jest.config.ts
+++ applications/leetcode-service/jest.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  moduleFileExtensions: ['ts'],
+  moduleFileExtensions: ['js', 'ts'],
   clearMocks: true,
 };
 
