a5f881d281bb22576cb3f360e000ab482f9e45b2
Fhoerth <federicohoerth@gmail.com>
2024-12-11 14:32:35 -0300

    Remove comment

--- applications/leetcode-service/project/main/modules/Scrapper/utils/convertImageToBase64.ts
+++ applications/leetcode-service/project/main/modules/Scrapper/utils/convertImageToBase64.ts
@@ -6,8 +6,6 @@ const convertImageToBase64 = async (imagePath: string): Promise<string> => {
     const base64String = imageBuffer.toString('base64');
     const mimeType = 'image/png';
 
-    // await unlink(imagePath);
-
     return `data:${mimeType};base64,${base64String}`;
   } catch (error) {
     throw new Error(`Error converting image \`${imagePath}\` to Base64`);
