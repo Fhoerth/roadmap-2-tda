34c95c9876742cd228f451b333a7030200beff61
Fhoerth <federicohoerth@gmail.com>
2024-12-13 16:01:40 -0300

    Remove default status

--- applications/leetcode-service/project/main/modules/LeetCodeScrapper/errors/LeetCodeError.ts
+++ applications/leetcode-service/project/main/modules/LeetCodeScrapper/errors/LeetCodeError.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
 class LeetCodeError extends Error {
-  public status = 404;
+  public status: number;
   public message: string;
 
   constructor(status: StatusCodes, message: string) {
